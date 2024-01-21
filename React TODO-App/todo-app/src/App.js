e2a3b105529601a0d38754728bae6dee1f3f7968
Armel <adjepang@yahoo.fr>
2024-01-21 12:42:29 +0100

    Reactapp

---
+++ React TODO-App/todo-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoList from './TodoList';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Meine Todo-Liste</h1>
+        <TodoList />
+      </header>
+    </div>
+  );
+}
+
+
+export default TodoList;
